70a196dc1d3947e760168c29deb7d3af6f4b3742
Anil <ay5741432@gmail.com>
2026-09-19 13:40:41 +0530

    add get all employees method in employee dao

--- dao/employee_dao.py
+++ dao/employee_dao.py
@@ -1,11 +1,21 @@
 from database.connection import Database
+from model.employee import Employee
 
 class EmployeeDao:
-    def getemployee(self):
-        # Code to retrieve employee data from the database
+    def get_all_employees(self):
+        # Code 
+        print("DAO getting the data")
         db = Database()
-        db.connect()
-        print("Retrieving employee data...")
+        conn = db.connect()
+        cursor = conn.cursor()
+        query = "select * from pdemployee1"
+        cursor.execute(query)
+        employees = []
+        for row in cursor.fetchall():
+            employee=Employee(row[0],row[1],row[2])
+            employees.append(employee)
+        conn.close()
+        return employees
         
     def save_employee(self, employee):
         # Code to save employee data to the database
