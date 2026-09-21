b0014479620669913b994c4e83fae991af4f23c3
Anil <ay5741432@gmail.com>
2026-09-21 11:30:52 +0530

    add repr method to employee model

--- model/employee.py
+++ model/employee.py
@@ -3,4 +3,6 @@ class Employee:
         self.id = id
         self.name = name
         self.salary = salary
-        
+
+    def __repr__(self):
+        return f"Employee(id={self.id}, name='{self.name}', salary={self.salary})"
