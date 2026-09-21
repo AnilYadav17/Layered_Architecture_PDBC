bb05f399c30443347e5d22873725830be3357b8b
Anil <ay5741432@gmail.com>
2026-09-21 11:30:52 +0530

    update product model with price and repr

--- model/product.py
+++ model/product.py
@@ -1,5 +1,8 @@
 class Product:
-    def __init__(self,id,name,salary):
+    def __init__(self, id, name, price):
         self.id = id
         self.name = name
-        self.salary = salary
+        self.price = price
+
+    def __repr__(self):
+        return f"Product(id={self.id}, name='{self.name}', price={self.price})"
